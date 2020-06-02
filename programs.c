8ee9c949fec97977673fed91075f03aa43160bcd
dhanyaflyhigh <65681739+dhanyaflyhigh@users.noreply.github.com>
2020-06-02 19:17:23 +0530

    strings added

--- programs.c
+++ programs.c
@@ -1,5 +1,10 @@
 welcome to c programming
 
-add two numbers
-prime number
-series
+#include<string.h>
+int main() {
+  char str[20]="abcdxyz";
+  int n1=strlen(str);
+  int n2=strlen("Hello World!");
+  printf("n1=%d,n2=%d\n",n1,n2);
+  return 0;
+}
