deaed02418c9cdf142041d43d359c3d1f5bae873
dhanyaflyhigh <65681739+dhanyaflyhigh@users.noreply.github.com>
2020-06-05 16:45:22 +0530

    sum printed ,display removed,

--- passingarray.c
+++ passingarray.c
@@ -5,7 +5,8 @@ int main() {
   int arr[10];
   fill(arr,n);
   sumarr(arr,n);
-  display(arr,n);
+printf("%d",sum);
+ // display(arr,n);
   
   return 0;
 }
@@ -15,11 +16,8 @@ void fill(int* parr,int n) {
     //*parr++=rand()%10;  
 }
 void sumarr(const int* parr, int n) {
-  int sum=0;
+int sum=0;
   for(int i=0;i<n;i++)
-    sum+=*parr++;   //sum+=parr[i]  //sum+=*(parr+i)
-}
-void display(const int *parr,int n) {
-  for(int i=0;i<n;i++)
-    printf("%d\n",parr[i]);  //*parr++, *(parr+i)
+ sum+=*parr++;
+
 }
