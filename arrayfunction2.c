fcd0307e5fa5e36a604074838f0ab1b5c1b445e5
dhanyaflyhigh <65681739+dhanyaflyhigh@users.noreply.github.com>
2020-06-05 20:11:18 +0530

    Create arrayfunction2.c

---
+++ arrayfunction2.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+void change_twod(int (*a)[3]);
+ 
+int main()
+{
+    int i,j, two_d[2][3] = {
+                               {99,44,11},
+                               {4,66,9}
+                           };
+ 
+    printf("Original array: \n\n");
+ 
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            printf("%3d ", two_d[i][j]);
+        }
+ 
+        printf("\n");
+    }
+ 
+    change_twod(two_d);
+ 
+    printf("\n\nModified array : \n\n");
+ 
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            printf("%3d ", two_d[i][j]);
+        }
+        printf("\n");
+    }
+ 
+    // signal to operating system everything works fine
+    return 0;
+}
+ 
+void change_twod(int (*arr)[3])
+{
+    int i, j;
+ 
+    printf("\n\nIncrementing every element by 5\n");
+    // increment original elements by 6
+ 
+    for(i = 0; i < 2; i++)
+    {
+        for(j = 0; j < 3; j++)
+        {
+            arr[i][j] = arr[i][j] + 5;
+        }
+    }
+ 
+}
